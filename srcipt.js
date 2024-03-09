9b79fa809e15f60ab23cf3641d6710517801bed1
Turasha <teonagiox.t@gmail.com>
2024-03-09 09:58:59 +0400

    30% done

---
+++ srcipt.js
@@ -0,0 +1,35 @@
+let item = 100;
+
+if (item < 50) {
+  console.log("ნაკლებია 50-ზე");
+} else {
+  console.log("შეცდომა");
+}
+
+let result1 = item < 50 ? "ნაკლებია 50-ზე" : "შეცდომა";
+console.log(result1);
+
+//--
+if (item > 20) {
+  console.log("მეტია 20-ზე");
+} else {
+  console.log("შეცდომა");
+}
+
+let result2 = item > 20 ? "მეტია 20-ზე" : "შეცდომა";
+console.log(result2);
+
+//--
+
+let person = "მარიამი";
+
+if ((person = "მარიამი")) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let result3 = (person = "მარიამი" ? true : false);
+console.log(result3);
+
+//--
